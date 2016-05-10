b2348106fa0d920f4e985c7653cac4bd2e507b08
dmustanger <dmustanger@thewickedclowns.net>
2016-05-10 09:34:44 -0500

    Fixed Motd not showing the correct name

--- ServerTools/src/Chat/Motd.cs
+++ ServerTools/src/Chat/Motd.cs
@@ -9,6 +9,7 @@
         {
             _message = _message.Replace("{PlayerName}", _cInfo.playerName);
             _cInfo.SendPackage(new NetPackageGameMessage(EnumGameMessages.Chat, string.Format("{0}{1}[-]", CustomCommands.ChatColor, _message), "Server", false, "", false));
+            _message = _message.Replace(_cInfo.playerName, "{PlayerName}");
         }
     }
 }
