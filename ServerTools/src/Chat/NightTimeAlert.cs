98b6f6aed8588f515c97ab574ac0af57229d4138
ObsComp <32458409+ObsComp@users.noreply.github.com>
2018-04-10 23:57:08 -0400

    Add files via upload

--- ServerTools/src/Chat/NightTimeAlert.cs
+++ ServerTools/src/Chat/NightTimeAlert.cs
@@ -9,14 +9,11 @@ namespace ServerTools
         {
             if (GameManager.Instance.World.IsDaytime())
             {
-                int _dusk = (int)SkyManager.GetDuskTime();
                 ulong _worldTime = GameManager.Instance.World.worldTime;
                 int _worldHours = (int)(_worldTime / 1000UL) % 24;
-                if (_worldHours < _dusk - 1)
-                {
-                    int _hours = _dusk - _worldHours;
-                    GameManager.Instance.GameMessageServer((ClientInfo)null, EnumGameMessages.Chat, string.Format("{0}{1} hours until night time[-]", Config.Chat_Response_Color, _hours), Config.Server_Response_Name, false, "ServerTools", false);
-                }
+                int _dusk = (int)SkyManager.GetDuskTime();
+                int _hours = _dusk - _worldHours;
+                GameManager.Instance.GameMessageServer((ClientInfo)null, EnumGameMessages.Chat, string.Format("{0}{1} hours until night time[-]", Config.Chat_Response_Color, _hours), Config.Server_Response_Name, false, "ServerTools", false);
             }
         }
     }
