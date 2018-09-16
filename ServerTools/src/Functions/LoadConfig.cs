dfbc251c6c71bcc7284174d539d62613b145c8d2
ObsessiveCompulsive <crobmail@hotmail.com>
2018-09-16 17:08:39 -0400

    Adjusted version to 11.1
    Set the config, howto, phrases and modinfo files to 11.1

--- ServerTools/src/Functions/LoadConfig.cs
+++ ServerTools/src/Functions/LoadConfig.cs
@@ -8,7 +8,7 @@ namespace ServerTools
         private const string configFile = "ServerToolsConfig.xml";
         private static string configFilePath = string.Format("{0}/{1}", API.ConfigPath, configFile);
         private static FileSystemWatcher fileWatcher = new FileSystemWatcher(API.ConfigPath, configFile);
-        public const double version = 11.0;
+        public const double version = 11.1;
         public static bool UpdateConfigs = false;
         public static string Server_Response_Name = "[FFCC00]ServerTools";
         public static string Chat_Response_Color = "[00FF00]";
